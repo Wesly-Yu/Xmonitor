1b9ee4b3c9b2b5e3b1e13c5bd983622ff701e358
Administrator <1633235633@qq.com>
2019-04-12 14:30:49 +0800

    model05

--- monitor/models.py
+++ monitor/models.py
@@ -69,6 +69,43 @@ class Userprofile(models.Model):
 
 class Strategy(models.Model):
     '''报警策略'''
+    trigger = models.ForeignKey('Trigger', verbose_name=u'所属触发器')
+    service = models.ForeignKey('Services', verbose_name=u'关联服务')
+    service_index = models.ForeignKey('ServiceIndex', verbose_name=u'关联服务指标')
+    specificed_index_key = models.CharField(verbose_name=u'只监控专门指定的指标key',max_length=64,blank=True,null=True)
+    operator_type_choices = (('equal','='),('lt','>'),('st','<'))             #lt:larger than ,st:small than
+    operator_type = models.CharField(u'运算符',choices=operator_type_choices,max_length=32)
+    data_calculator_type_choices =(('avg','Average'),
+                                   ('max','Max'),
+                                   ('hit','Hit'),
+                                   ('last','Last'),)
+    data_calculator_func = models.CharField(u'数据处理方式',choices=data_calculator_type_choices,max_length=64)
+    data_calculator_args = models.CharField(u'函数传入参数',help_text=u'若是多个参数，则用，号分开，第一个值是时间',max_length=64)
+    threshold = models.IntegerField(u'阈值')
+    logic_type_choices = (('or','OR'),('and','AND'))
+    logic_type = models.CharField(u"条件逻辑关系",choices=logic_type_choices,max_length=32,blank=True,null=True)
+    def __str__(self):
+        return "%s %s(%s(%s))" %(self.service_index,self.operator_type,self.data_calculator_func,self.data_calculator_args)
+
+
+class Trigger(models.Model):
+    '''告警级别'''
+    name = models.CharField(u'触发器名称',)
+    trigger_choices = ((1,'Information')
+                      ,(2,'Warning'),
+                      (3,'Average'),
+                      (4,'High'),
+                      (5,'Diaster'))
+    trigger_level = models.IntegerField(u'告警级别',choices=trigger_choices)
+    enable =models.BooleanField(default=True)
+    memo = models.TextField(u'备注',blank=True,null=True)
+    def __str__(self):
+        return self.name
+
+
+
+class Actions(models.Model):
+    '''定义触发告警后做什么操作'''
     name = models.CharField(max_length=64)
     triggers = models.ManyToManyField('Trigger',blank=True,help_text=u'触发哪些报警策略')
     hosts = models.ManyToManyField('Host',blank=True)
@@ -80,18 +117,3 @@ class Strategy(models.Model):
     enable = models.BooleanField(default=True)
     def __str__(self):
         return self.name
-
-
-
-class Trigger(models.Model):
-    '''告警级别'''
-    name = models.CharField(u'触发器名称',)
-    strigger_choices = ((1,'Information')
-                      ,(2,'Warning'),
-                      (3,'Average'),
-                      (4,'High'),
-                      (5,'Diaster'))
-
-
-
-class Actions(models.Model):
